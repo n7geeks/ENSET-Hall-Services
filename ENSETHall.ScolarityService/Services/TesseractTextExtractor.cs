9b53f8086da3774803db48a408da3118ac82de34
Youssef BEN SADIK <ysf.bensadik@gmail.com>
2023-03-27 02:05:44 +0000

    Update TesseractTextExtractor.cs

--- ENSETHall.ScolarityService/Services/TesseractTextExtractor.cs
+++ ENSETHall.ScolarityService/Services/TesseractTextExtractor.cs
@@ -11,8 +11,8 @@ public class TesseractTextExtractor : ITextExtractor
 {
 	public async Task<string?> ExtractAsync(IFormFile file, CancellationToken cancellationToken = default)
 	{
-		//try
-		//{
+		try
+		{
 			using var engine = new TesseractEngine(
 				Path.Combine(Directory.GetCurrentDirectory(), "tessdata"), 
 				"fra", 
@@ -30,11 +30,11 @@ public class TesseractTextExtractor : ITextExtractor
 					return page.GetText();
 				}
 			}
-		//}
-		//catch (Exception e)
-		//{
-		//	return null;
-		//}
+		}
+		catch (Exception e)
+		{
+			return e.Message;
+		}
 
 	}
 }
