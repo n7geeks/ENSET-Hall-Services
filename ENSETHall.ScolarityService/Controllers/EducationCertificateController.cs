66bb8b5ec6351fd27825553e36d5435b0d0e2f69
Youssef BEN SADIK <ysf.bensadik@gmail.com>
2023-03-27 21:39:16 +0000

    Add files via upload

--- ENSETHall.ScolarityService/Controllers/EducationCertificateController.cs
+++ ENSETHall.ScolarityService/Controllers/EducationCertificateController.cs
@@ -1,5 +1,7 @@
 using ENSETHall.ScolarityService.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ENSETHall.ScolarityService.Controllers;
 
@@ -20,21 +22,64 @@ public class EducationCertificateController : Controller
 
 	[HttpPost]
 	public async Task<IActionResult> ExtractStudentInfo(
-		[FromForm] IFormFile file, 
+		[FromForm] IFormFile? file, 
 		CancellationToken cancellationToken)
 	{
+		if (file is null)
+		{
+			return BadRequest(new
+			{
+				code = "MissingFile",
+				message = "The file is missing."
+			});
+		}
+		if (!file.ContentType.StartsWith("image/"))
+		{
+			return BadRequest(new
+			{
+				code = "InvalidFile",
+				message = "The file is not an image."
+			});
+		}
+		if (file.Length > 4 * 1024 * 1024)
+		{
+			return BadRequest(new
+			{
+				code = "InvalidFile",
+				message = "The file is too large."
+			});
+		}
 		var extractedText = await _textExtractor.ExtractAsync(file, cancellationToken);
 		if (extractedText is null)
 		{
-			return BadRequest();
+			return BadRequest(new
+			{
+				code = "TextExtractionError",
+				message = "Could not extract text from the image."
+			});
 		}
 		var result = await _studentInfoParser.ParseAsync(
 			extractedText, 
 			cancellationToken);
 		if (result is null)
 		{
-			return BadRequest();
+			return BadRequest(new
+			{
+				code = "TextParsingError",
+				message = "Could not parse the text."
+			});
 		}
-		return Ok(result);
+		var info = new
+		{
+			fullName = "",
+			cne = "",
+			cin = "",
+			promo = "",
+			major = "",
+			department = "",
+			diploma = "",
+			formation = ""
+		};
+		return Ok(JsonConvert.DeserializeAnonymousType(result, info));
 	}
 }
